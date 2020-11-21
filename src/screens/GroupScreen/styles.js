5d60df4d4736f2dfca18b2620638af472e87204d
Sandip Dutta <duttasandip11100@gmail.com>
2020-11-21 14:07:32 +0530

    snehasish success

---
+++ src/screens/GroupScreen/styles.js
@@ -0,0 +1,13 @@
+  
+import { StyleSheet } from 'react-native';
+/*import AppStyles from '../../config/styles';*/
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: 'white'
+    }
+});
+
+export default styles;
